ec6fcd80ab2f0c17e0a3939a147c21c1ba407c86
Luke <weidnerluke@gmail.com>
2020-05-20 20:28:11 -0400

    v1.0.8 reformatted global

--- index.js
+++ index.js
@@ -11,7 +11,18 @@ import UIBackgroundView from "./UIBackgroundView";
 import UIHeader from "./UIHeader";
 import UIScrollView from "./UIScrollView";
 
-import "./global.js";
+import nunitio from "typeface-nunitio";
+
+global.backgroundColor = "#03003b";
+global.lightBackgroundColor = "white";
+global.dark = false;
+
+global.gradientColor1 = "#7300CD";
+global.gradientColor2 = "#00F0FF";
+global.accentColor = "#2200ff";
+
+global.fontFamily = nunitio;
+global.boldFontFamily = nunitio;
 
 export {
   UIButton,
